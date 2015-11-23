134ca5fdc271e44eea0d80764768273e4eecffc6
Maksim Karpychev <mkarpychev@synergy.ru>
2015-11-23 09:07:31 +0300

    creating plugin

---
+++ ElcodiClearCacheBundle.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kodermax
+ * Date: 20.11.2015
+ * Time: 17:26
+ */
+
+namespace Elcodi\Plugin\ClearCacheBundle;
+use Symfony\Component\Console\Application;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+use Elcodi\Component\Plugin\Interfaces\PluginInterface;
+use Elcodi\Plugin\DisqusBundle\DependencyInjection\ElcodiDisqusExtension;
+
+
+class ElcodiClearCacheBundle extends Bundle implements PluginInterface
+{
+    /**
+     * Returns the bundle's container extension.
+     *
+     * @return ExtensionInterface The container extension
+     */
+    public function getContainerExtension()
+    {
+        return new ElcodiClearCacheExtension();
+    }
+
+    /**
+     * Register Commands.
+     *
+     * Disabled as commands are registered as services.
+     *
+     * @param Application $application An Application instance
+     *
+     * @return null
+     */
+    public function registerCommands(Application $application)
+    {
+        return null;
+    }
+}
